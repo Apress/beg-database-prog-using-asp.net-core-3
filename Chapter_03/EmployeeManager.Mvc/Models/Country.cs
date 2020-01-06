00ba6fc17abfd5ffe12fb2f5dee281977b991a42
redboofie <31323574+redboofie@users.noreply.github.com>
2020-01-06 19:26:23 +0000

    Update Country.cs
    In order to work with Northwind Database Country table Country.cs requires modifying as above.

--- Chapter_03/EmployeeManager.Mvc/Models/Country.cs
+++ Chapter_03/EmployeeManager.Mvc/Models/Country.cs
@@ -1,13 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace EmployeeManager.Mvc.Models
 {
+    [Table("Countries")]
     public class Country
     {
+        [Column("EmployeeID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "Country ID is required")]
+        [Display(Name = "Country ID")]
         public int CountryID { get; set; }
+
+
+        [Column("Name")]
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(80, ErrorMessage = "Name must be less than 80 characters")]
         public string Name { get; set; }
     }
 }
